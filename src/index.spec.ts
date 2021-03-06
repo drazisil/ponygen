0b6abcc80262d7c3b62481694abb6f0c27c2a070
Jo Becher <jwbecher@gmail.com>
2021-03-06 07:50:30 -0500

    chore: switch to jest because it just works for ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,15 +1,13 @@
 import request from "supertest";
-import tap from "tap";
 import server from "./";
 
-tap.test("Index", function (t) {
+test("Index", function () {
   request(server)
     .get("/about")
     .expect(200)
     .end((err, res) => {
       if (err) throw err;
-      t.contains(res.text, "About", '/about contains "About"');
-      t.done()
+      expect(res.text).toContain("About")
     });
 
 });
