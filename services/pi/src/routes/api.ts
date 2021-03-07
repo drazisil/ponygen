b7eba82abf346743f0ba699a7914940bbff06fe4
Jo Becher <jwbecher@gmail.com>
2021-03-07 14:42:21 -0500

    feat(pi): start crafting pony and color classes

--- services/pi/src/routes/api.ts
+++ services/pi/src/routes/api.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from "express";
 import got from "got";
+import { Pony } from "../Pony";
+
+export interface PIPonyJSON {
+  ID: number;
+  Name: string
+  BreedID: string
+  Gender: string
+  Colors: {
+    Eyes: string
+    Hair: string
+    Hair2: string
+    Body: string
+    Extra1: string
+    Extra2: string
+  };
+  Genes: string[];
+}
 
 export function apiHome(req: Request, res: Response): void {
   res.send("API birds");
@@ -12,7 +29,28 @@ export function apiRawHome(req: Request, res: Response): void {
 export async function apiRawPony(req: Request, res: Response): Promise<void> {
   const id = req.params.id;
 
+  const pony = await Pony.fetchById(Number.parseInt(id))
+  
+  const data = pony.asJSON()
+
+  res.json(data);
+}
+
+export async function getPony(id: number): Promise<PIPonyJSON> {
   const { body } = await got.get(`http://get.ponyisland.net?pony=${id}`, {
+    headers: {
+      "user-agent": `ponygen (https://github.com/drazisil/ponygen)`,
+    },
+  });
+  return JSON.parse(body);
+
+
+}
+
+export async function apiRawBreed(req: Request, res: Response): Promise<void> {
+  const id = req.params.id;
+
+  const { body } = await got.get(`http://get.ponyisland.net?breed=${id}`, {
     headers: {
       "user-agent": `ponygen (https://github.com/drazisil/ponygen)`,
     },
--- services/pi/src/routes/index.ts
+++ services/pi/src/routes/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { apiHome, apiRawHome, apiRawPony } from './api'
+import { apiHome, apiRawBreed, apiRawHome, apiRawPony } from './api'
 const router = express.Router()
 
 router.get('/api/raw/pony/:id', apiRawPony)
@@ -10,5 +10,8 @@ router.use('/api', apiHome)
 router.get('/about', (req, res) => {
   res.send('About birds')
 })
+
+router.get("/breed/:id", apiRawBreed);
+router.get("/pony/:id", apiRawPony);
 
 export default router
