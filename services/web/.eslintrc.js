27bc9d25cbc8f766f3ede3782f9c420a7e5d994c
Jo Becher <jwbecher@gmail.com>
2021-03-14 18:14:43 -0400

    feat(web)!: switch server to a single file
    * add webpack
    * remove typescript for web server

---
+++ services/web/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+    browser: true
+  },
+  extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': ['error', 'never', { sts: 'always' }],
+  },
+  settings: {
+    'import/extensions': [
+      '.js',
+      '.ts',
+    ],
+  },
+};
